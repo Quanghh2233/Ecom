c2d98adc06511363462c6faca1bff0c1e26cf321
Quanghh2233 <quanghoanghuy33@gmail.com>
2025-02-03 13:21:34 +0700

    update

---
+++ internal/controllers/Cart/cart.go
@@ -0,0 +1,17 @@
+package Cart
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Application struct {
+	prodCollection *mongo.Collection
+	userCollection *mongo.Collection
+}
+
+func NewApplication(prodCollection, userCollection *mongo.Collection) *Application {
+	return &Application{
+		prodCollection: prodCollection,
+		userCollection: userCollection,
+	}
+}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,47 +1,48 @@
 package route
 
-// import (
-// 	controllers "github.com/Quanghh2233/Ecommerce/internal/controllers/Adm"
-// 	Auth "github.com/Quanghh2233/Ecommerce/internal/controllers/Auth"
-// 	search "github.com/Quanghh2233/Ecommerce/internal/controllers/Search"
-// 	store "github.com/Quanghh2233/Ecommerce/internal/controllers/Store"
-// 	"github.com/Quanghh2233/Ecommerce/internal/middleware"
+import (
+	controllers "github.com/Quanghh2233/Ecommerce/internal/controllers/Admin"
+	Auth "github.com/Quanghh2233/Ecommerce/internal/controllers/Auth"
+	platform "github.com/Quanghh2233/Ecommerce/internal/controllers/PlatForm"
+	search "github.com/Quanghh2233/Ecommerce/internal/controllers/Search"
+	store "github.com/Quanghh2233/Ecommerce/internal/controllers/Store"
+	"github.com/Quanghh2233/Ecommerce/internal/middleware"
 
-// 	"github.com/gin-gonic/gin"
-// )
+	"github.com/gin-gonic/gin"
+)
 
-// func UserRoutes(incomingRoutes *gin.Engine) {
-// 	// Authentication routes
-// 	incomingRoutes.POST("/signup", Auth.Signup())
-// 	incomingRoutes.POST("/login", Auth.Login())
+func UserRoutes(incomingRoutes *gin.Engine) {
+	// Authentication routes
+	incomingRoutes.POST("/signup", Auth.Signup())
+	incomingRoutes.POST("/login", Auth.Login())
 
-// 	// Public routes
-// 	public := incomingRoutes.Group("")
-// 	{
-// 		public.GET("/view", controllers.SearchProduct())
-// 		public.GET("/filter", controllers.FilterProd())
-// 		public.GET("/search/product", search.SearchProductByQuery())
-// 		public.GET("/search/store", search.SearchStore())
-// 		public.GET("/store/:store_id", store.GetStore())
+	// Public routes
+	public := incomingRoutes.Group("")
+	{
+		public.GET("/view", platform.SearchProduct())
+		public.GET("/filter", platform.FilterProd())
+		public.GET("/search/product", search.SearchProductByQuery())
+		public.GET("/search/store", search.SearchStore())
+		public.GET("/store/:store_id", store.GetStore())
 
-// 	}
+	}
 
-// 	// Admin routes
-// 	adminRoutes := incomingRoutes.Group("/admin")
-// 	adminRoutes.Use(middleware.Authentication(), middleware.CheckPermission("admin"))
-// 	{
-// 		adminRoutes.DELETE("/deleteproduct/:product_id", controllers.DeleteProduct())
-// 		adminRoutes.POST("/delete_multiple", controllers.DelMultiple())
-// 		// adminRoutes.POST("/addproduct", controllers.ProductViewAdmin())
-// 		// adminRoutes.PUT("/updateproduct/:product_id", controllers.UpdateProduct())
-// 	}
+	// Admin routes
+	adminRoutes := incomingRoutes.Group("/admin")
+	adminRoutes.Use(middleware.Authentication(), middleware.CheckPermission("admin"))
+	{
+		adminRoutes.DELETE("/deleteproduct/:product_id", controllers.DeleteProduct())
+		adminRoutes.POST("/delete_multiple", controllers.DelMultiple())
+		// adminRoutes.POST("/addproduct", controllers.ProductViewAdmin())
+		// adminRoutes.PUT("/updateproduct/:product_id", controllers.UpdateProduct())
+	}
 
-// 	// Store routes
-// 	stores := incomingRoutes.Group("/store")
-// 	stores.Use(middleware.Authentication())
-// 	{
-// 		stores.POST("/:store_id/addproduct", middleware.CheckPermission("manage_products"), store.CreateProduct())
-// 		stores.DELETE("/:store_id/delete/:product_id", middleware.CheckPermission("manage_products"), store.DeleteProduct())
-// 		stores.PUT("/:store_id/product/:product_id", middleware.CheckPermission("manage_products"), store.UpdateProduct())
-// 	}
-// }
+	// Store routes
+	stores := incomingRoutes.Group("/store")
+	stores.Use(middleware.Authentication())
+	{
+		stores.POST("/:store_id/addproduct", middleware.CheckPermission("manage_products"), store.CreateProduct())
+		stores.DELETE("/:store_id/delete/:product_id", middleware.CheckPermission("manage_products"), store.DeleteProduct())
+		stores.PUT("/:store_id/product/:product_id", middleware.CheckPermission("manage_products"), store.UpdateProduct())
+	}
+}
